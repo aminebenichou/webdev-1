20c04e595ca2278055490e00ba4768aac0dd1f17
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2024-03-25 08:50:47 +0100

    third

--- main.js
+++ main.js
@@ -7,15 +7,37 @@ const options = {
     }
 };
 
+// vraibales
+
+const videos = document.getElementsByClassName("videos")[0]
+
+
 async function getVideos() {
     try {
         const response = await fetch(url, options);
-        const result = await response.text();
-        
-        console.log(result);
+        const result = await response.json();
+        for (let index = 0; index < result.videos.length; index++) {
+            const title = result.videos[index].title;
+            const author = result.videos[index].author;
+            // creating elements
+            var video = document.createElement('div');
+            video.className = "video-card"
+            var vid_title = document.createElement('h3');
+
+            // appending elements
+            
+            vid_title.innerHTML = title;
+            video.appendChild(vid_title);
+            
+            videos.appendChild(video);
+
+        }
+        console.log(result.videos[1]);
     } catch (error) {
         console.error(error);
     }
 }
+
+
 
 getVideos()
